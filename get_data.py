60848d3c9b7a92932255cf361f6a7f7c675291f3
dnstacy23 <dnstacy@crimson.ua.edu>
2024-04-03 17:53:44 -0400

    adds field goal percentage completion for each distance range

--- get_data.py
+++ get_data.py
@@ -28,6 +28,47 @@ def get_team_stats(year, team_id, stat_dict):
 def rank_teams_by_feature(stat_df, feature):
    return stat_df.sort_values(by=[feature])
 
+def calculate_field_goal_pct(stat_df):
+    field_goal_pct = defaultdict(list)
+    for i, row in stat_df.iterrows():
+        if row["fieldGoalAttempts1_19Kicking"] != 0.0:
+            pct1_19 = row["fieldGoalsMade1_19Kicking"] / row["fieldGoalAttempts1_19Kicking"]
+        else:
+            pct1_19 = 0.0
+        field_goal_pct["fieldGoalsMadePct1_19Kicking"].append(pct1_19)
+
+        if row["fieldGoalAttempts20_29Kicking"] != 0.0:
+            pct20_29 = row["fieldGoalsMade20_29Kicking"] / row["fieldGoalAttempts20_29Kicking"]
+        else:
+            pct20_29 = 0.0
+        field_goal_pct["fieldGoalsMadePct20_29Kicking"].append(pct20_29)
+
+        if row["fieldGoalAttempts30_39Kicking"] != 0.0:
+            pct30_39 = row["fieldGoalsMade30_39Kicking"] / row["fieldGoalAttempts30_39Kicking"]
+        else:
+            pct30_39 = 0.0
+        field_goal_pct["fieldGoalsMadePct30_39Kicking"].append(pct30_39)
+
+        if row["fieldGoalAttempts40_49Kicking"] != 0.0:
+            pct40_49 = row["fieldGoalsMade40_49Kicking"] / row["fieldGoalAttempts40_49Kicking"]
+        else:
+            pct40_49 = 0.0
+        field_goal_pct["fieldGoalsMadePct40_49Kicking"].append(pct40_49)
+
+        if row["fieldGoalAttempts50_59Kicking"] != 0.0:
+            pct50_59 = row["fieldGoalsMade50_59Kicking"] / row["fieldGoalAttempts50_59Kicking"]
+        else:
+            pct50_59 = 0.0
+        field_goal_pct["fieldGoalsMadePct50_59Kicking"].append(pct50_59)
+
+        if row["fieldGoalAttempts60_99Kicking"] != 0.0:
+            pct60_99 = row["fieldGoalsMade60_99Kicking"] / row["fieldGoalAttempts60_99Kicking"]
+        else:
+            pct60_99 = 0.0
+        field_goal_pct["fieldGoalsMadePct60_99Kicking"].append(pct60_99)
+
+    return field_goal_pct
+
 def main(argv):
     teams = get_teams()
     stat_dict = defaultdict(list)
@@ -40,6 +81,9 @@ def main(argv):
         stat_dict = get_team_stats(2023, team_id, stat_dict)
 
     stat_df = pd.DataFrame(stat_dict)
+    field_goal_pct = calculate_field_goal_pct(stat_df)
+    for key in field_goal_pct:
+        stat_df[key] = field_goal_pct[key]
 
 
 if __name__ == "__main__":
